f92ce3caea2f606d703dae5c2b440247ebc47d9d
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-11-27 23:40:36 +0700

    [andri] create 2 different fragment and finished setup

--- app/src/main/java/com/ridiansyah/papb_fragments/MainActivity.java
+++ app/src/main/java/com/ridiansyah/papb_fragments/MainActivity.java
@@ -1,7 +1,16 @@
 package com.ridiansyah.papb_fragments;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeAllFragment();
+    }
+
+    private void initializeAllFragment() {
+        FirstFragment firstFragment = new FirstFragment();
+        SecondFragment secondFragment = new SecondFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        int screenOrientation = getResources().getConfiguration().orientation;
+
+        //untuk portrait
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (screenOrientation == Configuration.ORIENTATION_LANDSCAPE){
+            fragmentTransaction.replace(R.id.fragment_left, firstFragment);
+            fragmentTransaction.replace(R.id.fragment_right, secondFragment);
+            Toast.makeText(getBaseContext(),"landscape", Toast.LENGTH_SHORT).show();
+        } else {
+            //fragmentTransaction.replace(R.id.fragment_right, secondFragment);
+            fragmentTransaction.add(R.id.container, firstFragment);
+            Toast.makeText(getBaseContext(),"portrait", Toast.LENGTH_SHORT).show();
+        }
+        fragmentTransaction.commit();
+        fragmentTransaction.addToBackStack(null);
     }
 }
---
+++ app/src/main/java/com/ridiansyah/papb_fragments/SecondFragment.java
@@ -0,0 +1,27 @@
+package com.ridiansyah.papb_fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SecondFragment extends Fragment {
+
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        return view;
+    }
+
+}
